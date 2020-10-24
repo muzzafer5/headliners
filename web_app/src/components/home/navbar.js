5587d3d507cce6676c12b3a960f6fd5bacd1bed8
Chakshu Gupta <gupta.18@iitj.ac.in>
2020-10-24 15:43:36 +0530

    [Update][Re-design] Home Nav-bar

--- web_app/src/components/home/navbar.js
+++ web_app/src/components/home/navbar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
-import { Nav, Navbar} from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 
 class Header extends Component {
-
     constructor(props) {
         super(props)
         this.state = {
-            errors:{}
+            errors: {}
         }
         this.logOut = this.logOut.bind(this)
     }
-
 
     logOut(e) {
         e.preventDefault();
@@ -19,30 +17,20 @@ class Header extends Component {
     }
 
     render() {
-       
         return (
-            <Navbar className="px-3" collapseOnSelect expand="lg"
-                style={{
-                    borderBottom: "1px solid #e6f7dc",
-                }}
-            >
-                <Navbar.Brand href="/">
-                    <span
-                        style={{
-                            color: "#6FA843",
-                            fontSize: "20px",
-                            fontWeight: "500"
-                        }}
-                    >
-                        Outsourcing App
-            </span>
-                </Navbar.Brand>
+            <Navbar className="px-5 navbar-dark bg-dark" collapseOnSelect expand="lg">
+                <Navbar.Brand href="/">Headliner</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto"></Nav>
                     <Nav>
-                        <Nav.Link eventKey={1} >Profile</Nav.Link>
-                        <Nav.Link eventKey={2} onClick={this.logOut} className="ml-3">Logout</Nav.Link>
+                        <Nav.Link 
+                            eventKey={1} 
+                            className="btn btn-outline-secondary mx-2 disabled">Profile</Nav.Link>
+                        <Nav.Link 
+                            eventKey={2} 
+                            className="btn btn-outline-secondary mx-2" 
+                            onClick={this.logOut}>Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
