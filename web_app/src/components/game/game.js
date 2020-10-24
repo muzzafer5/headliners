81ee411597607cb03de6ee7cc6e79844c9aa5e81
Chakshu Gupta <gupta.18@iitj.ac.in>
2020-10-25 00:38:34 +0530

    [Add] Feedback layout in Game

--- web_app/src/components/game/game.js
+++ web_app/src/components/game/game.js
@@ -1,133 +1,191 @@
 import React, { Component } from 'react'
-import {fetch_news} from './ConnectServer'
-import {Button, Modal } from 'react-bootstrap'
-import bg_pic from '../../static/news2.jpg' 
+import axios from 'axios'
+import MultiSelect from "react-multi-select-component";
 
 class Game extends Component {
     constructor() {
         super()
         this.state = {
-            show : 0,
+            category: 'business',
             errors: {},
-            show_headlines : false,
             news: {}
         }
         this.onCloseModal = this.onCloseModal.bind(this)
         this.onNextModal = this.onNextModal.bind(this)
     }
     componentDidMount() {
-        // fetch_news().then(res=>{
-        //     console.log(res)
-        // })
         if (!localStorage.usertoken)
             this.props.history.push(`/login`)
     }
-    onCloseModal(){
+    onCloseModal() {
         this.props.history.push(`/home`)
     }
- 
-    onNextModal(){
-        if(this.state.show === 0){
-            this.setState({show_headlines : true})
-            fetch_news({ category: 'sports'}).then(res=>{
-                console.log(res)
-                this.setState({news:res})
-            })
+
+    onNextModal() {
+        if (this.state.category === 'business') {
+            this.setState({ category: 'entertainment' })
         }
-        if (this.state.show === 1) {
-            fetch_news({ category: 'politics' }).then(res => {
-                console.log(res)
-                this.setState({ news: res })
-            })
+        else if (this.state.category === 'entertainment') {
+            this.setState({ category: 'health' })
         }
-        if (this.state.show === 2) {
-            fetch_news({ category: 'science' }).then(res => {
-                console.log(res)
-                this.setState({ news: res })
-            })
+        else if (this.state.category === 'health') {
+            this.setState({ category: 'science' })
         }
-        if (this.state.show === 3) {
-            fetch_news({ category: 'entertainment' }).then(res => {
-                console.log(res)
-                this.setState({ news: res })
-            })
+        else if (this.state.category === 'science') {
+            this.setState({ category: 'sports' })
         }
-        if (this.state.show === 4) {
-            fetch_news({ category: 'technology' }).then(res => {
-                console.log(res)
-                this.setState({ news: res })
-            })
+        else if (this.state.category === 'sports') {
+            this.setState({ category: 'technology' })
         }
-        if (this.state.show === 5) {
-            fetch_news({ category: 'health' }).then(res => {
-                console.log(res)
-                this.setState({ news: res })
-            })
-        }
-        if (this.state.show === 6) {
-            this.setState({show_headlines : false})
-        }
-        this.setState({show: this.state.show + 1})
+        var category = this.state.category
+        var url = 'https://newsapi.org/v2/top-headlines?country=in&category='.concat(category.concat('&apiKey=e72f2023fa894d99944fb610c2fc31dd'))
+        axios.get(url).then(res => {
+            console.log(res['data']['articles'][0])
+            this.setState({ news: res['data']['articles'][0] })
+        })
     }
 
     render() {
-        const consentModal = (
-            <Modal centered show={this.state.show === 0} animation={false}>
-                <Modal.Header closeButton onClick={this.onCloseModal}>
-                    <Modal.Title>Headline News Phase 1</Modal.Title>
-                </Modal.Header>
-                <Modal.Body >
-                    <div style={{ color: "#bd4a4a", fontSize : "14px"}}>
-                        * News Headlines are being pulled from the most popular stations.
-                        Swipe left (provide a left arrow for emphasis) if you would not share the headline to your social media community or right 
-                        (provide a right arrow for emphasis) if you would share the headline. *
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="dark" className="btn btn-primary" onClick={this.onNextModal}>
-                        Enter
-          </Button>
-                </Modal.Footer>
-            </Modal>
-        )
+        var category = this.state.category
+        var url = 'https://newsapi.org/v2/top-headlines?country=in&category='.concat(category.concat('&apiKey=e72f2023fa894d99944fb610c2fc31dd'))
+        axios.get(url).then(res => {
+            console.log(res)
+            this.setState({ news: res['data']['articles'][0] })
+        })
+        var button;
+        if (this.state.category === 'technology') {
+            button = <button onClick="" class="btn btn-primary">End</button>
+        }
+        else {
+            button = <button onClick={this.onNextModal} class="btn btn-primary">Next</button>
+        }
         const headline = (
-            <Modal centered size = "lg" show={this.state.show_headlines} animation={false}>
-                <Modal.Header closeButton onClick={this.onCloseModal}>
-                    <Modal.Title>Headline News Phase 1</Modal.Title>
-                </Modal.Header>
-                <Modal.Body >
-                    <div style = {{height : "500px"}}>
-                        {this.state.news ?
-                            (
-                                <div>
-                                        <div className = "text-center pt-3" style = {{fontSize : "20px", fontWeight : "700"}}> Title: </div> 
-                                        <div style={{ fontSize : "18px", color: "#4e4f4f"}}>{this.state.news.title}</div>
-                                        <div className="text-center pt-5" style={{ fontSize: "20px", fontWeight: "700" }}>Desctiption: </div> 
-                                    <div style={{ fontSize: "18px", color: "#4e4f4f" }}>{this.state.news.description}</div>
+            <div
+                className="container"
+                style={{ width: "100vw" }}>
+                <div className="row justify-content-center">
+                    <div class="card shadow-lg col-lg-6 col-md-8 col-10 my-5">
+                        <div class="card-body">
+                            <h2 class="card-header mb-3 text-capitalize">{this.state.category}</h2>
+                            <div className="row">
+                                <div className="col-12 my-2" style={{ order: 1 }}>
+                                    <div class="card col-12">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{this.state.news.title}</h5>
+                                            <p class="card-text">{this.state.news.content}</p>
+                                            <div class="card bg-LIGHT mb-3">
+                                                <div class="card-body">
+                                                    <div class="row border-light">
+                                                        <div className="col-8 my-auto py-auto">
+                                                            Will you share this news?
+                                                        </div>
+                                                        <div className="col-4">
+                                                            <div class="form-group">
+                                                                <select class="form-control" id="share">
+                                                                    <option>Yes</option>
+                                                                    <option>No</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="card bg-LIGHT mb-3">
+                                                <div class="card-body">
+                                                    <div class="row border-light">
+                                                        <div className="col-8 my-auto py-auto">
+                                                            Will you mark this news fake?
+                                                        </div>
+                                                        <div className="col-4">
+                                                            <div class="form-group">
+                                                                <select class="form-control" id="share">
+                                                                    <option>Yes</option>
+                                                                    <option>No</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="card bg-LIGHT mb-3">
+                                                <div class="card-body">
+                                                    <div class="mb-2">Involved Cultural Values</div>
+                                                    <MultiSelect
+                                                        required
+                                                        name="culturalValues"
+                                                        // value="{this.state.cultural_values}"
+                                                        // onChange={cultural_values => this.setState({ cultural_values })}
+                                                        options={[{value: 'Value 1', label: 'Value 1'}, {value: 'Value 2', label: 'Value 2'}]}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            ):''}
+                                <div className="col-12 my-2" style={{ order: 2 }}>
+                                    <div class="card col-12">
+                                        <img class="card-img-top" src={this.state.news.urlToImage} alt="News Headline"></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">{this.state.news.title}</h5>
+                                            <p class="card-text">{this.state.news.description}</p>
+                                            <div class="card bg-LIGHT mb-3">
+                                                <div class="card-body">
+                                                    <div class="row border-light">
+                                                        <div className="col-8 my-auto py-auto">
+                                                            Will you share this news?
+                                                        </div>
+                                                        <div className="col-4">
+                                                            <div class="form-group">
+                                                                <select class="form-control" id="share">
+                                                                    <option>Yes</option>
+                                                                    <option>No</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="card bg-LIGHT mb-3">
+                                                <div class="card-body">
+                                                    <div class="row border-light">
+                                                        <div className="col-8 my-auto py-auto">
+                                                            Will you mark this news fake?
+                                                        </div>
+                                                        <div className="col-4">
+                                                            <div class="form-group">
+                                                                <select class="form-control" id="share">
+                                                                    <option>Yes</option>
+                                                                    <option>No</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="card bg-LIGHT mb-3">
+                                                <div class="card-body">
+                                                    <div class="mb-2">Involved Cultural Values</div>
+                                                    <MultiSelect
+                                                        required
+                                                        name="culturalValues"
+                                                        // value="{this.state.cultural_values}"
+                                                        // onChange={cultural_values => this.setState({ cultural_values })}
+                                                        options={[{value: 'Value 1', label: 'Value 1'}, {value: 'Value 2', label: 'Value 2'}]}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {button}
+                        </div>
                     </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="dark" className="btn btn-primary" onClick={this.onNextModal}>
-                        Next
-          </Button>
-                </Modal.Footer>
-            </Modal>
+                </div>
+            </div>
         )
-        var sectionStyle = {
-            width: "100%",
-            height: "750px",
-            backgroundImage: "url(" + bg_pic + ")",
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            opacity: "1"
-        };
         return (
-            <div className="home" style = {sectionStyle}>
-                Game
-                {consentModal}
+            <div style={{ width: "100vw", minHeight: "100vh", fontFamily: "Lato" }}>
                 {headline}
             </div>
         )
