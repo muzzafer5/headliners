951cd6447c7559b28379dded17aed45fa444c84d
Chakshu Gupta <gupta.18@iitj.ac.in>
2020-10-25 00:47:33 +0530

    [Update][Design] Landing page

--- web_app/src/components/auth/landing.js
+++ web_app/src/components/auth/landing.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
-import landing_pic from '../../static/news2.jpg'
 
 class Landing extends Component {
   componentDidMount() {
@@ -8,49 +7,34 @@ class Landing extends Component {
       this.props.history.push(`/home`)
   }
   render() {
-    var sectionStyle = {
-      width: "100%",
-      height: "750px",
-      backgroundImage: "url(" + landing_pic + ")",
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      opacity: "1"
-    };
     return (
-      <div className="landing" style={sectionStyle}>
-        <div className="header px-3"
-          style={
-            {
-              borderBottom: "1px solid #e6f7dc"
-            }
-          } >
-          <Navbar collapseOnSelect expand="lg" >
-            <Navbar.Brand href="/" >
-              <span style={
-                {
-                  color: "#6FA843",
-                  fontSize: "20px",
-                  fontWeight: "500"
-                }
-              } >
-                Outsourcing App </span>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" >
-              <Nav className="mr-auto" > </Nav>
-              <Nav >
-                <Nav.Link href="/auth/login" > <b>Login</b> </Nav.Link>
-                <Nav.Link eventKey={2}
-                  href="/auth/signup" > <b>Signup</b> </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-        <div className="text-center mt-5" 
-        >
-          <h1>News App</h1>
-
+      <div>
+        <Navbar
+          className="px-5 navbar-dark bg-dark"
+          collapseOnSelect
+          expand="lg"
+          style={{ height: "10vh" }}>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto"></Nav>
+            <Nav>
+              <Nav.Link
+                eventKey={1}
+                className="btn btn-outline-secondary mx-2"
+                href={'/auth/login/'}>Login</Nav.Link>
+              <Nav.Link
+                eventKey={2}
+                className="btn btn-outline-secondary mx-2"
+                href={'/auth/signup/'}>Signup</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div style={{ width: "100vw", minHeight: "90vh" }} className="container-fluid">
+          <div className="d-flex flex-column justify-content-center" style={{ minHeight: "90vh" }}>
+            <div className="d-flex flex-row justify-content-center">
+              <h1>Headliner</h1>
+            </div>
+          </div>
         </div>
       </div>
     )
