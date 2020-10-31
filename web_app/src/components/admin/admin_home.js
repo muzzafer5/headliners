2602c2865304f5e0ad8c5fe0971f5be715680876
Chakshu Gupta <chakshugupta2000@gmail.com>
2020-10-31 18:36:51 +0530

    [Update][Design] Admin Home

--- web_app/src/components/admin/admin_home.js
+++ web_app/src/components/admin/admin_home.js
@@ -13,30 +13,35 @@ class AdminHome extends Component {
             this.props.history.push(`/admin`)
     }
 
-    render(){
-        return(
-            <div >
-                <div className = "container">
-                    <div style = {{textAlign : "center", margin : "50px 200px"}}>
-                        <button className="btn btn-dark" style = {{width:"100%"}} onClick={() => this.props.history.push('/admin/users')}>
-                            User details
+    render() {
+        return (
+            <div className="d-flex justify-content-center py-2" style={{ width: "100vw", minHeight: "90vh" }}>
+                <div className="d-flex flex-column col-10 justify-content-center shadow-lg">
+                    <div className="d-flex flex-row mb-1 justify-content-center">
+                        <button 
+                            className="btn btn-info col-lg-4 col-md-6 col-sm-8 col-10" 
+                            onClick={() => this.props.history.push('/admin/users')}>
+                            User Data
                         </button>
                     </div>
-                    <div style={{ textAlign: "center", margin: "50px 200px" }}>
-                        <button className="btn btn-dark" style={{ width: "100%" }}  onClick={() => this.props.history.push('/admin/age_distribution')}>
-                            Age distribution
-                            </button>                            
+                    <div className="d-flex flex-row mb-1 justify-content-center">
+                        <button 
+                            className="btn btn-info col-lg-4 col-md-6 col-sm-8 col-10" 
+                            onClick={() => this.props.history.push('/admin/age_distribution')}>
+                            Age Distribution
+                        </button>
                     </div>
-                    <div style={{ textAlign: "center", margin: "50px 200px" }}>
-                        <button className="btn btn-dark" style={{ width: "100%" }}  onClick={() => this.props.history.push('/admin/gender_distribution')}>
-                            Gender distribution
-                            </button>
+                    <div className="d-flex flex-row mb-1 justify-content-center">
+                        <button 
+                            className="btn btn-info col-lg-4 col-md-6 col-sm-8 col-10" 
+                            onClick={() => this.props.history.push('/admin/gender_distribution')}>
+                            Gender Distribution
+                        </button>
                     </div>
                 </div>
             </div>
         )
     }
-
 }
 
 export default AdminHome
